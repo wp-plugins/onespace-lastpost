48e514dea14bc5d7bcdbeb06e790fd5940c39b7a
igrossiter <igrossiter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-13 19:20:42 +0000

    cleaning before implementing new features
    git-svn-id: https://plugins.svn.wordpress.org/onespace-lastpost/tags/0.2.0@965245 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/class-os-lastpost-printer.php
+++ core/class-os-lastpost-printer.php
@@ -3,19 +3,19 @@
  * Version: 0.1
  * Author: Ian Garcez
  * Author URI: http://onespace.com.br
- * License: 
- * 
+ * License:
+ *
  * Copyright 2013  Ian Garcez  (email : ian@onespace.com.br)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as 
+ * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -90,17 +90,22 @@ class Os_lastpost_printer{
 			$this->querys_for_print[] = new WP_Query($query_args);
 		}
 
-		
+
 	}
 
 	private function makeHtml(){
 		$this->html = '<div class="os-lastpost">';
 
+		// save the current post for recovery later
+		global $post;
+		$save_post = $post;
+
 		foreach ($this->querys_for_print as $q) {
 			$this->html .= '<ul>';
 			if ($q->have_posts()): while($q->have_posts()):	$q->the_post();
+					$post = $q->post;
 					$this->html .= '<li>';
-					$this->makeHtmlWithInfo($q->post);
+					$this->makeHtmlWithInfo();
 					$this->html .= '</li>';
 				endwhile;
 			endif;
@@ -108,30 +113,32 @@ class Os_lastpost_printer{
 		}
 		$this->html .= '</div>';
 
+		$post = $save_post;
+
 	}
 
-	private function makeHtmlWithInfo($p) {
-		foreach ($this->info_to_print as $i) {
-			if(strpos($i, 'title') !== false){
-				$this->html .= '<h2 class="title"><a href="' . get_permalink($p->ID) . '">';
-				$this->html .= get_the_title($p->ID).'</a></h2>';
+	private function makeHtmlWithInfo() {
+		foreach ($this->info_to_print as $info) {
+			if(strpos($info, 'title') !== false){
+				$this->html .= '<h2 class="title"><a href="' . get_permalink() . '">';
+				$this->html .= get_the_title().'</a></h2>';
 			}
 
-			if(strpos($i, 'date') !== false){
-				$format = $this->getDateFormat($i);
+			if(strpos($info, 'date') !== false){
+				$format = $this->getDateFormat($info);
 				$this->html .= '<p class="date">'. get_the_time($format) . '</p>';
 			}
 
-			if(strpos($i, 'excerpt') !== false){
-				$this->html .= $this->getTrimContentFromInfo($i, $p);
+			if(strpos($info, 'excerpt') !== false){
+				$this->html .= $this->getTrimContentFromInfo($info);
 			}
 
-			if(strpos($i, 'content') !== false){
-				$this->html .= $this->getFullContentForInfo($i, $p);
+			if(strpos($info, 'content') !== false){
+				$this->html .= $this->getFullContentForInfo($info);
 			}
 
-			if(strpos($i, 'thumb') !== false){
-				$this->html .= $this->getThumbsForInfo($i, $p);
+			if(strpos($info, 'thumb') !== false){
+				$this->html .= $this->getThumbsForInfo($info);
 			}
 		}
 	}
@@ -145,42 +152,42 @@ class Os_lastpost_printer{
 		}
 	}
 
-	private function getTrimContentFromInfo($string, $p) {
+	private function getTrimContentFromInfo($string) {
 		$options = explode(':', $string);
 		if (count($options) > 1){
 			$max_char = intval($options[1]);
 			$text = ($options[2]);
 
-			return $this->trimContent($p, $max_char, $text);
+			return $this->trimContent($max_char, $text);
 		} else {
-			return $this->trimContent($p);
+			return $this->trimContent();
 		}
 
 	}
 
-	private function trimContent($p, $max_char = 60, $text = ' ...Read more') {
-		$content = $p->post_content;
+	private function trimContent($max_char = 60, $text = ' ...Read more') {
+		$content = get_the_content();
 		if (strlen($content) > $max_char - strlen($text)){
 			$content = substr($content, 0, $max_char) . $text;
 		}
 
 		$content = strip_tags($content);
-		
-		return '<p><a href="' . get_permalink($p->ID) . '">' . $content . '</a></p>';
+
+		return '<p><a href="' . get_permalink() . '">' . $content . '</a></p>';
 	}
 
-	private function getFullContentForInfo($string, $p){
-		$content = $p->post_content;
+	private function getFullContentForInfo($string){
+		$content = get_the_content();
 		$content = apply_filters('the_content', $content);
 		return $content;
 	}
 
-	private function getThumbsForInfo($string, $p){
+	private function getThumbsForInfo($string){
 		$string = explode(':', $string);
 		$size = intval($string[1]);
 		$size = array($size, $size);
 		if ($size > 10){
-			return '<a href="' . get_permalink($p->ID) . '">' .get_the_post_thumbnail( $p->ID, $size ) . '</a>';
+			return '<a href="' . get_permalink() . '">' .get_the_post_thumbnail(get_the_ID(), $size ) . '</a>';
 		} else {
 			return $this->printError("Tamanho da imagem inválido");
 		}
